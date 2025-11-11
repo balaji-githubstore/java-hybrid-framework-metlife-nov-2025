d48cfb38ef9c0bc6f3ee039770d3a605d4e9bbda
balaji-githubstore <balaji0017@gmail.com>
2025-11-11 15:14:26 +0530

    Updated invalidLoginTest

--- src/test/java/com/metlife/test/LoginUITest.java
+++ src/test/java/com/metlife/test/LoginUITest.java
@@ -2,14 +2,8 @@ package com.metlife.test;
 
 import com.metlife.base.AutomationWrapper;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class LoginUITest extends AutomationWrapper {
 
