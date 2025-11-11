a11e08a2d4eced7c9edf655fb203232bb68a485f
balaji-githubstore <balaji0017@gmail.com>
2025-11-11 13:53:56 +0530

    First Commit Added LoginUITest

---
+++ src/test/java/com/metlife/test/LoginUITest.java
@@ -0,0 +1,40 @@
+package com.metlife.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LoginUITest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void titleTest() {
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, "OrangeHRM");
+    }
+
+    @Test
+    public void headerTest() {
+        String actualHeader = driver.findElement(By.xpath("//h5[contains(normalize-space(),'Log')]")).getText();
+        Assert.assertEquals(actualHeader, "Login");
+    }
+}
