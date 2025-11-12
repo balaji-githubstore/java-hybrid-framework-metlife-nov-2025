85a1da009c47be8fc942ba4d2a99f15bc2d867ed
balaji-githubstore <balaji0017@gmail.com>
2025-11-12 15:39:32 +0530

    Working with excel

--- src/test/java/com/metlife/utilities/ExcelUtils.java
+++ src/test/java/com/metlife/utilities/ExcelUtils.java
@@ -9,20 +9,20 @@ import java.io.IOException;
 
 public class ExcelUtils {
 
-    public static Object[][] getSheetIntoTwoDimensionalArray(String filePath,String sheetname) throws IOException {
+    public static Object[][] getSheetIntoTwoDimensionalArray(String filePath, String sheetname) throws IOException {
         FileInputStream file = new FileInputStream(filePath);
         XSSFWorkbook book = new XSSFWorkbook(file);
         XSSFSheet sheet = book.getSheet(sheetname);
 
-        int rowCount=sheet.getPhysicalNumberOfRows();
-        int cellCount=sheet.getRow(0).getPhysicalNumberOfCells();
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        int cellCount = sheet.getRow(0).getPhysicalNumberOfCells();
 
-        Object[][] data=new Object[rowCount-1][cellCount];
+        Object[][] data = new Object[rowCount - 1][cellCount];
 
-        DataFormatter formatter=new DataFormatter();
+        DataFormatter formatter = new DataFormatter();
         for (int r = 1; r < rowCount; r++) {
             for (int c = 0; c < cellCount; c++) {
-                data[r-1][c]=formatter.formatCellValue(sheet.getRow(r).getCell(c));
+                data[r - 1][c] = formatter.formatCellValue(sheet.getRow(r).getCell(c));
             }
         }
         book.close();
