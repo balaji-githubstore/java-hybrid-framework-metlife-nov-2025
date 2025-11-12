6f67735bd870e6cb081c25bb5cf4c98beedf21ae
balaji-githubstore <balaji0017@gmail.com>
2025-11-12 17:10:19 +0530

    Updated the code

--- src/test/java/com/metlife/test/LoginTest.java
+++ src/test/java/com/metlife/test/LoginTest.java
@@ -3,9 +3,13 @@ package com.metlife.test;
 import com.metlife.base.AutomationWrapper;
 import com.metlife.utilities.DataSource;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class LoginTest extends AutomationWrapper {
 
@@ -18,7 +22,6 @@ public class LoginTest extends AutomationWrapper {
         String actualValue = driver.findElement(By.xpath("//p[contains(normalize-space(),'Quick')]")).getText();
         Assert.assertEquals(actualValue, expectedValue);
     }
-
 
     @Test(dataProviderClass = DataSource.class,dataProvider = "commonDataProvider",groups = {"regression"})
     public void invalidLoginTest(String username, String password, String expectedError) {
