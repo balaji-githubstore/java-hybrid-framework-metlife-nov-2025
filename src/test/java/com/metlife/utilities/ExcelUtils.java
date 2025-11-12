9dc911591ef4061fb1c568163a159391b4d2d6e5
balaji-githubstore <balaji0017@gmail.com>
2025-11-12 15:38:38 +0530

    Working with excel

---
+++ src/test/java/com/metlife/utilities/ExcelUtils.java
@@ -0,0 +1,32 @@
+package com.metlife.utilities;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ExcelUtils {
+
+    public static Object[][] getSheetIntoTwoDimensionalArray(String filePath,String sheetname) throws IOException {
+        FileInputStream file = new FileInputStream(filePath);
+        XSSFWorkbook book = new XSSFWorkbook(file);
+        XSSFSheet sheet = book.getSheet(sheetname);
+
+        int rowCount=sheet.getPhysicalNumberOfRows();
+        int cellCount=sheet.getRow(0).getPhysicalNumberOfCells();
+
+        Object[][] data=new Object[rowCount-1][cellCount];
+
+        DataFormatter formatter=new DataFormatter();
+        for (int r = 1; r < rowCount; r++) {
+            for (int c = 0; c < cellCount; c++) {
+                data[r-1][c]=formatter.formatCellValue(sheet.getRow(r).getCell(c));
+            }
+        }
+        book.close();
+        file.close();
+        return data;
+    }
+}
